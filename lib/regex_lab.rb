7e37252825ace11292906285436059995f322aa3
Ming Duan <11203duan@gmail.com>
2019-02-27 02:13:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,9 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  arr = []
-  arr << text.scan(/^(un)\w+$(ing)/)
-  arr
+  text.scan(/^(un)\w+$(ing)/)
 end
 
 def words_five_letters_long(text)
